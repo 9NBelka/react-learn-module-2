25c08e2beeb20f79dc211d1bc5149d1894893bb0
Nikita <belka.nik02@gmail.com>
2024-07-05 21:34:45 +0300

    несколько состояний в обьекте

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,17 +3,28 @@ import { useState } from "react"
 
 function App() {
 
-const [clicks, setClicks] = useState(0);
+const [clicks, setClicks] = useState({
+  a: 0,
+  b:0
+});
 
-const handleCliks = ()  =>{
-  setClicks(clicks + 1)
-}
+const handleCliks = (value) =>{
+  setClicks({
+    ...clicks,
+    [value]: clicks[value] + 1
+  });
+};
+
+// const handleCliks = ()  =>{
+//   setClicks(clicks + 1)
+// }
 
 
   return (
     <>
-    <ClickTracker value={clicks} onTrack={handleCliks}>Clicks 1: </ClickTracker>
-    <ClickTracker value={clicks} onTrack={handleCliks}>Clicks 2: </ClickTracker>
+    <ClickTracker value={clicks.a} onTrack={ () => handleCliks("a")}>Clicks 1: </ClickTracker>
+    <ClickTracker value={clicks.b} onTrack={ () => handleCliks("b")}>Clicks 2: </ClickTracker>
+    <p>{clicks.a + clicks.b}</p>
     </>
   )
 }
